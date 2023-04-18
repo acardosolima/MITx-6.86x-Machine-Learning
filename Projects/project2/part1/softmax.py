1f152f4a061340c2863d57af986ed2b5b91e0417
Adriano <adrianocardoso1991@gmail.com>
2023-04-18 04:20:03 -0300

    Changed folder structure

--- Projects/project2/part1/softmax.py
+++ Projects/project2/part1/softmax.py
@@ -5,15 +5,11 @@ from utils import *
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.sparse as sparse
-
-
 def augment_feature_vector(X):
     """
     Adds the x[i][0] = 1 feature for each data point x[i].
-
     Args:
         X - a NumPy matrix of n data points, each with d - 1 features
-
     Returns: X_augment, an (n, d) NumPy array with the added feature for each datapoint
     """
     column_of_ones = np.zeros([len(X), 1]) + 1
@@ -23,7 +19,6 @@ def compute_probabilities(X, theta, temp_parameter):
     """
     Computes, for each datapoint X[i], the probability that X[i] is labeled as j
     for j = 0, 1, ..., k-1
-
     Args:
         X - (n, d) NumPy array (n datapoints each with d features)
         theta - (k, d) NumPy array, where row j represents the parameters of our model for label j
@@ -31,13 +26,18 @@ def compute_probabilities(X, theta, temp_parameter):
     Returns:
         H - (k, n) NumPy array, where each entry H[j][i] is the probability that X[i] is labeled as j
     """
-    #YOUR CODE HERE
+    e_expon = np.matmul(theta, np.transpose(X) / temp_parameter)
+    c = np.amax(e_expon, axis = 0)
+    h_numerator = np.exp(e_expon - c)
+    h_denomenator = np.sum(np.exp(e_expon -c), axis = 0)
+    H = np.divide(h_numerator, h_denomenator)
+    return H
     raise NotImplementedError
+
 
 def compute_cost_function(X, Y, theta, lambda_factor, temp_parameter):
     """
     Computes the total cost over every datapoint.
-
     Args:
         X - (n, d) NumPy array (n datapoints each with d features)
         Y - (n, ) NumPy array containing the labels (a number from 0-9) for each
@@ -46,17 +46,40 @@ def compute_cost_function(X, Y, theta, lambda_factor, temp_parameter):
                 model for label j
         lambda_factor - the regularization constant (scalar)
         temp_parameter - the temperature parameter of softmax function (scalar)
-
     Returns
         c - the cost value (scalar)
     """
-    #YOUR CODE HERE
-    raise NotImplementedError
+
+    # Objective function
+
+    probability = np.clip(compute_probabilities(X, theta, temp_parameter), 1e-15, 1 - 1e-15)
+    M = sparse.coo_matrix(([1]*X.shape[0], (Y, range(X.shape[0]))), shape = (theta.shape[0],X.shape[0])).toarray()
+    cost = -1/X.shape[0] * (M * np.log(probability)).sum()
+    regularization = np.square(theta).sum()
+    c = cost + (lambda_factor / 2) * regularization
+    return c
+
+#    cost = 0
+#    probability = np.clip(compute_probabilities(X, theta, temp_parameter), 1e-15, 1 - 1e-15)
+#    for i in range(X.shape[0]):
+#        for j in range(theta.shape[0]):
+#            if Y[i] == j:
+#                cost += np.log(probability[j, i])
+#    cost = -1/X.shape[0] * cost
+#    # Regularization term
+#    sum_theta_2 = 0
+#    for i in range(theta.shape[0]):
+#        for j in range(theta.shape[1]):
+#            sum_theta_2 += np.square(theta[i, j])
+#    c = cost + (lambda_factor / 2) * sum_theta_2
+#    print(cost)
+#    return c
+#    raise NotImplementedError
+
 
 def run_gradient_descent_iteration(X, Y, theta, alpha, lambda_factor, temp_parameter):
     """
     Runs one step of batch gradient descent
-
     Args:
         X - (n, d) NumPy array (n datapoints each with d features)
         Y - (n, ) NumPy array containing the labels (a number from 0-9) for each
@@ -66,37 +89,44 @@ def run_gradient_descent_iteration(X, Y, theta, alpha, lambda_factor, temp_param
         alpha - the learning rate (scalar)
         lambda_factor - the regularization constant (scalar)
         temp_parameter - the temperature parameter of softmax function (scalar)
-
     Returns:
         theta - (k, d) NumPy array that is the final value of parameters theta
     """
-    #YOUR CODE HERE
+    # Sparse matrix used for mapping Y[i] and j labels (Same functionality as Y[i] = j, but vectorized)
+    M = sparse.coo_matrix(([1]*X.shape[0], (Y, range(X.shape[0]))), shape = (theta.shape[0],X.shape[0])).toarray()
+    probability = np.clip(compute_probabilities(X, theta, temp_parameter), 1e-15, 1-1e-15)
+    # Cost function with labels being considered (Y[i] == j - probability)
+    gradient_without_x = np.subtract(M, probability)
+    gradient = np.matmul(gradient_without_x, X)
+    gradient = -1/(temp_parameter * X.shape[0]) * gradient + lambda_factor * theta
+    # regularization term
+    updated_theta = theta - (alpha * gradient)
+    return updated_theta
     raise NotImplementedError
 
 def update_y(train_y, test_y):
     """
     Changes the old digit labels for the training and test set for the new (mod 3)
     labels.
-
     Args:
         train_y - (n, ) NumPy array containing the labels (a number between 0-9)
                  for each datapoint in the training set
         test_y - (n, ) NumPy array containing the labels (a number between 0-9)
                 for each datapoint in the test set
-
     Returns:
         train_y_mod3 - (n, ) NumPy array containing the new labels (a number between 0-2)
                      for each datapoint in the training set
         test_y_mod3 - (n, ) NumPy array containing the new labels (a number between 0-2)
                     for each datapoint in the test set
     """
-    #YOUR CODE HERE
+    train_y_mod3 = np.mod(train_y, 3)
+    test_y_mod3 = np.mod(test_y, 3)
+    return train_y_mod3, test_y_mod3
     raise NotImplementedError
 
 def compute_test_error_mod3(X, Y, theta, temp_parameter):
     """
     Returns the error of these new labels when the classifier predicts the digit. (mod 3)
-
     Args:
         X - (n, d - 1) NumPy array (n datapoints each with d - 1 features)
         Y - (n, ) NumPy array containing the labels (a number from 0-2) for each
@@ -104,11 +134,12 @@ def compute_test_error_mod3(X, Y, theta, temp_parameter):
         theta - (k, d) NumPy array, where row j represents the parameters of our
                 model for label j
         temp_parameter - the temperature parameter of softmax function (scalar)
-
     Returns:
         test_error - the error rate of the classifier (scalar)
     """
-    #YOUR CODE HERE
+    predicted_labels = get_classification(X, theta, temp_parameter)
+    mod_predicted_labels = np.mod(predicted_labels, 3)
+    return 1 - np.mean(mod_predicted_labels == Y)
     raise NotImplementedError
 
 def softmax_regression(X, Y, temp_parameter, alpha, lambda_factor, k, num_iterations):
@@ -117,7 +148,6 @@ def softmax_regression(X, Y, temp_parameter, alpha, lambda_factor, k, num_iterat
     with theta initialized to the all-zeros array. Here, theta is a k by d NumPy array
     where row j represents the parameters of our model for label j for
     j = 0, 1, ..., k-1
-
     Args:
         X - (n, d - 1) NumPy array (n data points, each with d-1 features)
         Y - (n, ) NumPy array containing the labels (a number from 0-9) for each
@@ -127,7 +157,6 @@ def softmax_regression(X, Y, temp_parameter, alpha, lambda_factor, k, num_iterat
         lambda_factor - the regularization constant (scalar)
         k - the number of labels (scalar)
         num_iterations - the number of iterations to run gradient descent (scalar)
-
     Returns:
         theta - (k, d) NumPy array that is the final value of parameters theta
         cost_function_progression - a Python list containing the cost calculated at each step of gradient descent
@@ -143,13 +172,11 @@ def softmax_regression(X, Y, temp_parameter, alpha, lambda_factor, k, num_iterat
 def get_classification(X, theta, temp_parameter):
     """
     Makes predictions by classifying a given dataset
-
     Args:
         X - (n, d - 1) NumPy array (n data points, each with d - 1 features)
         theta - (k, d) NumPy array where row j represents the parameters of our model for
                 label j
         temp_parameter - the temperature parameter of softmax function (scalar)
-
     Returns:
         Y - (n, ) NumPy array, containing the predicted label (a number between 0-9) for
             each data point
