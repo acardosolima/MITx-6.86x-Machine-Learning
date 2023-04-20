696296fb86aa65f4ab02ab2b4122d8e3283e9335
Adriano <adrianocardoso1991@gmail.com>
2023-04-20 07:01:46 -0300

    Implemented project 4.2 exercise

---
+++ Projects/project4/netflix/main.py
@@ -0,0 +1,22 @@
+import numpy as np
+import kmeans
+import common
+import naive_em
+import em
+
+X = np.loadtxt("toy_data.txt")
+
+K = [1, 2, 3, 4]
+seeds = [0, 1, 2, 3, 4] 
+
+costs_kmeans = [0]*5
+mixtures_kmeans = [0]*5
+posts_kmeans = [0]*5
+
+for k in range(len(K)):
+    for i in range(len(seeds)):
+        
+        mixtures_kmeans[i], posts_kmeans[i], costs_kmeans[i] = \
+        kmeans.run(X, *common.init(X, K[k], seeds[i]))
+    
+    print("Cluster: ", k+1, "- Cost: ", np.min(costs_kmeans))
